fd80c843640bf69c20fc67db6c32850348034df8
bessgeor <bessgeor@gmail.com>
2017-11-11 17:26:10 +0300

    Added a struct to represent travel card

---
+++ plazius-test-task/TravelCard.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace plazius_test_task
+{
+	public struct TravelCard : IEquatable<TravelCard>
+	{
+		public TravelCard( string departureFrom, string arriveTo )
+		{
+			DepartureFrom = departureFrom;
+			ArriveTo = arriveTo;
+		}
+
+		public string DepartureFrom { get; }
+		public string ArriveTo { get; }
+
+		public override int GetHashCode()
+		{
+			unchecked
+			{
+				const int prime = -1521134295;
+				int hash = 12345701;
+				hash = hash * prime + System.Collections.Generic.EqualityComparer<string>.Default.GetHashCode( DepartureFrom );
+				hash = hash * prime + System.Collections.Generic.EqualityComparer<string>.Default.GetHashCode( ArriveTo );
+				return hash;
+			}
+		}
+
+		public bool Equals( TravelCard other ) => DepartureFrom == other.DepartureFrom && ArriveTo == other.ArriveTo;
+		public override bool Equals( object obj ) => obj is TravelCard && Equals( (TravelCard) obj );
+
+		public static bool operator ==( TravelCard x, TravelCard y ) => x.Equals( y );
+		public static bool operator !=( TravelCard x, TravelCard y ) => !x.Equals( y );
+	}
+}
