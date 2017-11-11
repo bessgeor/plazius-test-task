78ac8293ca3628f37413a3adacd8bc26191d76cd
bessgeor <bessgeor@gmail.com>
2017-11-11 17:35:43 +0300

    Wrote basic unit tests

---
+++ tests/TravelCardArrayExtensionsTests.cs
@@ -0,0 +1,67 @@
+﻿using plazius_test_task;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace plazius_test_task_tests
+{
+	[TestFixture]
+	public class TravelCardArrayExtensionsTests
+	{
+		[Test]
+		public void NullArrayIsThrowing()
+			=> ( (Action) ( () => ( (TravelCard[]) null ).Sort() ) )
+			.ShouldThrow<ArgumentNullException>();
+
+		private class UnsortedAndSortedTravelCardArraysSource : IEnumerable<TestCaseData>
+		{
+			public IEnumerator<TestCaseData> GetEnumerator()
+			{
+				return EnumerateRealCases()
+					.Select( c => new TestCaseData( c.unsorted, c.sorted ).SetName( c.name ) )
+					.GetEnumerator();
+
+				IEnumerable<(TravelCard[] unsorted, TravelCard[] sorted, string name)> EnumerateRealCases()
+				{
+					yield return (Array.Empty<TravelCard>(), Array.Empty<TravelCard>(), "Empty arrays are handled properly");
+					yield return (new[] { MakeCard( "Stockholm", "Oslo" ) }, new[] { MakeCard( "Stockholm", "Oslo" ) }, "Array of one element is sorted");
+					yield return
+					(
+						new[] { MakeCard( "Stockholm", "Oslo" ), MakeCard( "Västerås", "Stockholm" ) },
+						new[] { MakeCard( "Västerås", "Stockholm" ), MakeCard( "Stockholm", "Oslo" ) },
+						"Array of two elements is sorted"
+					);
+					yield return
+					(
+						new[] { MakeCard( "Stockholm", "Oslo" ), MakeCard( "Västerås", "Stockholm" ), MakeCard( "Oslo", "Trondheim" ) },
+						new[] { MakeCard( "Västerås", "Stockholm" ), MakeCard( "Stockholm", "Oslo" ), MakeCard( "Oslo", "Trondheim" ) },
+						"Array of three elements is sorted"
+					);
+					yield return
+					(
+						new[] { MakeCard( "Stockholm", "Oslo" ), MakeCard( "Trondheim", "Reykjavík" ), MakeCard( "Västerås", "Stockholm" ), MakeCard( "Oslo", "Trondheim" ) },
+						new[] { MakeCard( "Västerås", "Stockholm" ), MakeCard( "Stockholm", "Oslo" ), MakeCard( "Oslo", "Trondheim" ), MakeCard( "Trondheim", "Reykjavík" ) },
+						"Array of four elements is sorted"
+					);
+
+					TravelCard MakeCard( string from, string to ) // just a shortcut
+						=> new TravelCard( from, to );
+				}
+			}
+
+			IEnumerator IEnumerable.GetEnumerator()
+				=> GetEnumerator();
+		}
+
+		[Test]
+		[TestCaseSource( typeof( UnsortedAndSortedTravelCardArraysSource ) )]
+		public void SortIsSorting( TravelCard[] unsorted, TravelCard[] sorted )
+			=> unsorted.Sort()
+			.Should().ContainInOrder( sorted ) // asserts only order disregarding of consecutiveness
+			.And.Subject
+			.Should().HaveCount( sorted.Length ); // forces consecutiveness
+	}
+}
---
+++ tests/TravelCardTests.cs
@@ -0,0 +1,42 @@
+﻿using plazius_test_task;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+
+namespace plazius_test_task_tests
+{
+	[TestFixture]
+	public class TravelCardTests
+	{
+		[Test]
+		[TestCase( null, "Oslo", TestName = "Departure city is null" )]
+		[TestCase( "Oslo", null, TestName = "Arrival city is null" )]
+		public void NullCityIsThrowing( string from, string to )
+			=> AssertException<ArgumentNullException>( from, to );
+
+		[Test]
+		[TestCase( "", "Oslo", "Empty city is not allowed: departureFrom", TestName = "Departure city is empty" )]
+		[TestCase( "Oslo", "", "Empty city is not allowed: arriveTo", TestName = "Arrival city is empty" )]
+		[TestCase( "paris", "Oslo", "Lowercase-starting city is not allowed: departureFrom", TestName = "Departure city is starting with lowercase letter" )]
+		[TestCase( "Oslo", "paris", "Lowercase-starting city is not allowed: arriveTo", TestName = "Arrival city is starting with lowercase letter" )]
+		[TestCase( "Oslo", "Oslo", "Arrival and departure cities should not match", TestName = "Arrival city is the same as departure one" )]
+		public void InvalidCityIsThrowing( string from, string to, string message )
+			=> AssertException<ArgumentException>( from, to )
+			.WithMessage( message );
+
+		[Test]
+		public void TravelCardIsCreatedAndFilledOnValidData()
+		{
+			const string departureFrom = "Oslo";
+			const string arriveTo = "Paris";
+			TravelCard card = new TravelCard( departureFrom, arriveTo );
+			card.DepartureFrom.Should().Be( departureFrom );
+			card.ArriveTo.Should().Be( arriveTo );
+		}
+
+		private FluentAssertions.Specialized.ExceptionAssertions<TException> AssertException<TException>( string from, string to )
+			where TException : Exception
+			=> ( (Action) ( () => new TravelCard( from, to ) ) )
+			.ShouldThrow<TException>();
+	}
+}
